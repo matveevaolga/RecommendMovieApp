4c5cec00417c5b948ebd1efd8c66d5c3a3341cff
matveevaolga <matveevaolga2882@gmail.com>
2025-06-28 00:45:44 +0300

    Initial Commit

---
+++ RecommendMovieApp/RecommendMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendMovieAppApp.swift
+//  RecommendMovieApp
+//
+//  Created by Olga Matveeva on 28.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecommendMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
